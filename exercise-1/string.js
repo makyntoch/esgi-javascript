50a556bd8b14051302983966ab04de3f22c62289
Mikael P <mpaul@wedigital.garden>
2019-11-27 11:03:59 +0100

    feat: create verlan function

--- exercise-1/string.js
+++ exercise-1/string.js
@@ -7,12 +7,12 @@ function ucfirst(str) {
     return str[0].toUpperCase() + str.substring(1);
 }
 
-console.log(ucfirst('test ucfirst'));
+console.log(ucfirst("test ucfirst"));
 
 
 function capitalize(str) {
     if (!validString(str)) return "";
-    return str.toLowerCase().split(' ').map(item => ucfirst(item)).join(" ");
+    return str.toLowerCase().split(" ").map(item => ucfirst(item)).join(" ");
 }
 
 console.log(capitalize("hello world capitalize"));
@@ -22,7 +22,7 @@ function camelCase(str){
     return str
       .replace(/[^a-zA-Z0-9 ]/gi, " ")
       .toLowerCase()
-      .split(' ')
+      .split(" ")
       .map(item => ucfirst(item))
       .join("");
 }
@@ -30,15 +30,15 @@ console.log(camelCase("hel?lo_wor/ld caMel case 1"));
 
 function snake_case(str){
     if (!validString(str)) return "";
-    return str.replace(/[^a-zA-Z0-9\\_ ]/gi, "").toLowerCase().split(' ').map(item => item).join("_");
+    return str.replace(/[^a-zA-Z0-9\\_ ]/gi, "").toLowerCase().split(" ").map(item => item).join("_");
 }
 
-console.log(snake_case("ToggleCase is_the coolest"));
 console.log(snake_case("hel?lo WorL/d sna!!ke ca:se9"));
 
 function leet(str){
     if (!validString(str)) return "";
-    return str.replace(/[a]/gi, 4)
+    return str
+    .replace(/[a]/gi, 4)
     .replace(/[e]/gi, 3)
     .replace(/[i]/gi, 1)
     .replace(/[o]/gi, 0)
@@ -47,3 +47,10 @@ function leet(str){
 }
 
 console.log(leet("a e i o u y A E I O U Y"));
+
+function verlan(str){
+  if(!validString(str)) return "";
+  return str.split(" ").map(item => item.split("").reverse().join("")).join(" ")
+}
+
+console.log(verlan("Hello world"));
