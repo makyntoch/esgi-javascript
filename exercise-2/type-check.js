0c050aad24977d12ccc7d46d87639a6d9b5d7be3
Mikael P <mpaul@wedigital.garden>
2019-11-27 12:55:05 +0100

    Feat(exercise-2): Create type_check_v1 function

---
+++ exercise-2/type-check.js
@@ -0,0 +1,19 @@
+function type_test_v1(input, type){
+    switch(input){
+        case null:
+            return type === "null"
+        case Array:
+            return type === "Array"
+        default:
+            return type === String(typeof input)
+    }
+}
+
+console.log(type_test_v1(null, "null"))
+console.log(type_test_v1(Array, "Array"))
+console.log(type_test_v1(1, "number"))
+console.log(type_test_v1("eric", "string"))
+console.log(type_test_v1(1.05, "float"))
+
+
+
