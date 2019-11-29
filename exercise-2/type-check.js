3a135b35a440b75cacb978322dcccac2774e7c1b
Mikael P <mpaul@wedigital.garden>
2019-11-29 17:07:13 +0100

    feat: type_check_v2 begin

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -1,11 +1,25 @@
 function type_check_v1(input, type){
-    if(Array.isArray(input)) return type === "array";
-    if(input === null) return type === "null";
-    return type === typeof input;
+  if(Array.isArray(input)) return type === "array";
+  if(input === null) return type === "null";
+  return type === typeof input;
 }
 
 console.log(type_check_v1(null, "null"))
 console.log(type_check_v1([1,2,3], "array"))
 console.log(type_check_v1(1, "number"))
 
+function type_check_v2(vari, conf){
+  for(key in conf){
+    switch(key){
+      case 'type':
+        if(!type_check_v1(vari, conf[key])) return false;
+        break;
+      case 'value':
 
+        break;
+      case 'enum':
+        break;    
+    }
+  }
+  return true;
+}
