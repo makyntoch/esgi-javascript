31c20b7b787a7ff823e50c11383a2bf8e5c80f98
Mikael P <mpaul@wedigital.garden>
2019-11-29 14:42:30 +0100

    feat: remove switch case method

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -1,20 +1,22 @@
 function type_check_v1(input, type){
-    switch(input){
-        case null:
-            return type === "null"
-        case Array:
-            return type === "array"
-        default:
-            return type === typeof input
-    }
+    // switch(input){
+    //     case null:
+    //         return type == "null"
+    //     case Array:
+    //         return type == "array"
+    //     default:
+    //         return type == typeof input
+    // }
+    if(Array.isArray(input)) return type == "array";
+    if(input == null) return type == "null";
+    return type == typeof input;
 }
 
 console.log(type_check_v1(null, "null"))
-console.log(type_check_v1(Array, "array"))
+console.log(type_check_v1([1,2,3], "array"))
 console.log(type_check_v1(1, "number"))
 console.log(type_check_v1("eric", "string"))
-console.log(type_check_v1(1.05, "float"))
-
+console.log(type_check_v1(1.05, "number"))
 console.log(type_check_v1(undefined, "undefined"))
 
 
