32ce18b50849f32b4dba63018b285026a0c80330
Mikael P <mpaul@wedigital.garden>
2019-11-29 14:51:55 +0100

    :zap: Clean type check function and test

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -1,12 +1,4 @@
 function type_check_v1(input, type){
-    // switch(input){
-    //     case null:
-    //         return type == "null"
-    //     case Array:
-    //         return type == "array"
-    //     default:
-    //         return type == typeof input
-    // }
     if(Array.isArray(input)) return type === "array";
     if(input === null) return type === "null";
     return type === typeof input;
@@ -15,8 +7,5 @@ function type_check_v1(input, type){
 console.log(type_check_v1(null, "null"))
 console.log(type_check_v1([1,2,3], "array"))
 console.log(type_check_v1(1, "number"))
-console.log(type_check_v1("eric", "string"))
-console.log(type_check_v1(1.05, "number"))
-console.log(type_check_v1(undefined, "undefined"))
 
 
